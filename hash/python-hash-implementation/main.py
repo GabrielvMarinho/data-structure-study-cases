c4eec067e6ac662cf84c34888a2f32b4f91d9661
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2025-06-11 23:58:40 -0300

    working on open addressing hashmap implemetation

--- hash/python-hash-implementation/main.py
+++ hash/python-hash-implementation/main.py
@@ -3,8 +3,7 @@ import hashlib
 class HashTable:
     def __init__(self, size):
         self.size = size
-        self.hash_table = self.create_buckets()
-
+        self.hash_table = [[("",{})] for i in range(self.size)]
 
     def get_position(self, key):
         encoded_key = key.encode("utf-8")
@@ -12,75 +11,80 @@ class HashTable:
         int_number = int.from_bytes(sha_256_key)
         hashed_key =  int_number % self.size
         return hashed_key
+    
+    def get_next_free_position(self, key, val, position):
 
-    def create_buckets(self):
-        return [[] for _ in range(self.size)]
+        if position <= self.size-1: 
+            bucket = self.hash_table[position]
+
+            if bucket != [] or bucket == [("-", {})]:
+                return self.get_next_free_position(key, val, position+1)
+            else:
+                return position
+                
+        else:
+            print("not enough spaces! recalculate")
     
     def set_val(self, key, val):
 
-        
         hashed_key = self.get_position(key)
-
         bucket = self.hash_table[hashed_key]
 
-        found_key = False
-
-        for index, record in enumerate(bucket):
-            record_key, record_val = record
-
-            if record_key == key:
-                found_key = True
-                break
-
-        if found_key:
-            bucket[index] = (key, val)
+    
+        if bucket == []:
+            bucket.insert(hashed_key, (key, val))
+        elif bucket != [(key, val)]:
+            position = self.get_next_free_position(key, val, hashed_key)
+            self.hash_table[position] = [(key, val)]
         else:
-            bucket.append((key, val))
+            print("already exist")
+            
+    def get_next_position_equals(self, key, position):
+
+        key_bucket, value_bucket = self.hash_table[position][0]
+        if position <= self.size-1:
+          
+            if(key_bucket != "-" or key_bucket != key):
+                return self.get_next_position_equals(key, position+1)                    
+            else:
+                return position
+
+        else:
+            print("not enough spaces! recalculate")
 
     def get_val(self, key):
         hashed_key = self.get_position(key)
 
         bucket = self.hash_table[hashed_key]
-        
+        key_bucket, value_bucket = bucket[0]
 
-
-        found_key = False
-        for index, record in enumerate(bucket):
-            record_key, record_val = record
-
-            if record_key == key:
-                found_key = True
-                break
-
-        if found_key:
-            return record_val
+        if(bucket):
+            if(key_bucket == key):
+                return bucket
+            else:
+                position = self.get_next_position_equals(key, hashed_key)
+                return self.hash_table[position]
         else:
             return "no record found"
-        
+    
     def delete_val(self, key):
         hashed_key = self.get_position(key)
-
-        bucket = self.hash_table[hashed_key]
-
-        found_key = False
-        for index, record in enumerate(bucket):
-            record_key, record_val = record
-
-            if record_key == key:
-                found_key = True
-                break
-        if found_key:
-            bucket.pop(index)
-        return
+        key_bucket, value_bucket = self.hash_table[hashed_key][0]
+        if(key_bucket == key):
+            self.hash_table[hashed_key] = [("-",{})]
+        else:
+            self.hash_table[self.get_next_position_equals(key, hashed_key)] = [("-",{})]
+            
     def __str__(self):
         return "".join(str(item) for item in self.hash_table)
     
-hash_tab = HashTable(50)
+hash_tab = HashTable(3)
+
+
 
 hash_tab.set_val("gabriel@gmail.com", {})
 
-print(hash_tab.get_position("gabriel@gmail.com"))
-print(hash_tab.get_val("gabriel@gmail.com"))
+
+
+
 print(hash_tab)
-
-
